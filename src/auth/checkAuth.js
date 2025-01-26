af2f48e173d60a9a3003d991941321165937149c
Vasyl Meleshyn <vasyl.aspedia@gmail.com>
2025-01-26 17:41:58 +0200

    feat: authorization added

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+export async function checkAuth(req, reply) {
+  try {
+    if (req.url === '/login' || req.url === '/signup') {
+      return;
+    }
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return reply.status(401).send({ error: 'Unauthorized' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+  } catch (err) {
+    return reply.status(401).send({ error: 'Unauthorized' });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
-import Fastify from 'fastify'
+import Fastify from 'fastify';
 import mercurius from 'mercurius';
 import prisma from './prisma/index.js';
-import { createUser } from './auth/createUser.js';
 
-import 'dotenv/config'
+import 'dotenv/config';
 
 import schema from './schema/index.js';
 import resolvers from './resolvers/index.js';
 
-const app = Fastify()
+import { createUser } from './auth/createUser.js';
+import { loginUser } from './auth/loginUser.js';
+import { checkAuth } from './auth/checkAuth.js';
+
+const app = Fastify();
 
 app.register(mercurius, {
   schema,
   resolvers,
   graphiql: true,
-})
+});
 
-app.post('/signup', createUser)
+app.post('/signup', createUser);
+app.post('/login', loginUser);
+
+app.addHook('onRequest', checkAuth)
 
 app.post('/', async function (req, reply) {
   const query = req.body.query;
-  const variables = req.body.variables
-  return reply.graphql(query, { prisma }, variables)
-})
+  const variables = req.body.variables;
+  return reply.graphql(query, { prisma, user: req.user }, variables);
+});
 
-app.listen({ port: process.env.PORT })
+app.listen({ port: process.env.PORT });
