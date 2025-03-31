cf9dd129244c21d483d744ea2ae5d4a55b0d1db5
Vasyl Meleshyn <vasjanja@gmail.com>
2025-03-31 19:52:07 +0300

    feat: introduced simplest roles

--- src/auth/loginUser.js
+++ src/auth/loginUser.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 import prisma from '../prisma/index.js';
 
 export async function loginUser(req, reply) {
-  const { email, password } = req.body;
+  const { email, password } = JSON.parse(req.body);
   const user = await prisma.user.findUnique({ where: { email } });
   if (!user) {
     return reply.status(401).send({ error: 'Invalid email or password' });
@@ -13,10 +13,13 @@ export async function loginUser(req, reply) {
   if (!isPasswordValid) {
     return reply.status(401).send({ error: 'Invalid email or password' });
   }
-
-  const token = jwt.sign({ user }, process.env.JWT_SECRET, {
-    expiresIn: '7d',
-  });
-
+  const tokenizedUser = { id: user.id, role: user.role, name: user.name }
+  const token = jwt.sign(
+    tokenizedUser,
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '7d',
+    }
+  );
   return reply.send({ token });
 }
--- src/auth/verifyToken.js
+++ src/auth/verifyToken.js
@@ -1,30 +1,25 @@
 import prisma from '../prisma/index.js';
 import jwt from 'jsonwebtoken';
 
-function normalizeUser(user) {
-  return Object.entries(user).reduce((normalizedUser, [key, value]) => {
-    if (key === 'updatedAt') return normalizedUser;
-    if (key === 'password') return normalizedUser;
-    normalizedUser[key] = value;
-    return normalizedUser;
-  }, {});
-}
-
 export async function verifyToken(req, reply) {
   try {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
       return reply.status(401).send({ error: 'Unauthorized' });
     }
-
+    
     const token = authHeader.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    if (decoded.userId) {
-      const user = await prisma.user.findUnique({
-        where: { id: decoded.userId },
+    
+    if (decoded.user) {
+      const isExisting = await prisma.user.findUnique({
+        where: { id: decoded.user.id },
       });
-      return { data: normalizeUser(user) };
+      if (isExisting) {
+        return {user: decoded.user, valid: true }
+      } else {
+        throw Error('Does not exist')
+      }
     }
 
     throw Error('Incorrect');
