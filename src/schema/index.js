1cd8c125462d7fded55d6fca421a0ffe5d8c810c
Vasyl Meleshyn <vasjanja@gmail.com>
2025-04-03 21:59:33 +0300

    feat: added lesson data to graphql #20

--- src/schema/index.js
+++ src/schema/index.js
@@ -3,46 +3,6 @@ const { mutation } = require('./mutationType.js');
 const { inputTypes } = require('./inputTypes.js');
 
 const schema = `
-  
-  ${query}
-
-  ${mutation}
-
-  ${inputTypes}
-  
-
-  type Lesson {
-    id: String,
-    subject: String, 
-    teacher: Teacher,
-    pupils: [Pupil]
-  }
-
-  type Pupil {
-    id: String,
-    name: String,
-    birthday: Date,
-    group: String,
-    lessons: [Lesson]
-    user: User
-  }
-
-  type Teacher {
-    id: String, 
-    name: String,
-    lessons: [Lesson]
-    user: User
-  }
-
-  type User {
-    id: String,
-    email: String,
-    name: String,
-    role: Role,
-    pupil: Pupil,
-    teacher: Teacher
-  }
-
   scalar Date
 
   enum Role {
@@ -51,6 +11,64 @@ const schema = `
     GUEST
     ADMIN
     SUPER_ADMIN
+  }
+
+  enum LessonContentType {
+    TEXT
+    VIDEO
+    AUDIO
+    IMAGE
+    PDF
+    LINK
+  }
+  ${query}
+
+  ${mutation}
+
+  ${inputTypes}
+  
+
+  type Lesson {
+    id: String
+    subject: String 
+    teacher: Teacher
+    pupils: [Pupil]
+    category: String
+    objective: String
+    title: String
+    content: [LessonContent]
+  }
+
+  type Pupil {
+    id: String
+    name: String
+    birthday: Date
+    group: String
+    lessons: [Lesson]
+    user: User
+  }
+
+  type Teacher {
+    id: String 
+    name: String
+    lessons: [Lesson]
+    user: User
+  }
+
+  type User {
+    id: String
+    email: String
+    name: String
+    role: Role
+    pupil: Pupil
+    teacher: Teacher
+  }
+
+  type LessonContent {
+    id: String
+    type: LessonContentType
+    order: Int
+    value: String
   }
 `;
 
