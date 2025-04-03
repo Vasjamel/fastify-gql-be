e596c9f8703974ee444289572f5f54d3bd06d841
Vasyl Meleshyn <vasjanja@gmail.com>
2025-04-03 22:08:44 +0300

    feat: lesson populated with lessonContent

--- src/resolvers/utils/includes.js
+++ src/resolvers/utils/includes.js
@@ -3,6 +3,7 @@
 const LESSONS_INCLUDE = {
   teacher: { include: { lessons: true, user: true } },
   pupils: { include: { lessons: true, user: true } },
+  content: true
 };
 
 const PUPILS_INCLUDE = {
@@ -10,13 +11,14 @@ const PUPILS_INCLUDE = {
     include: {
       teacher: { include: { lessons: true } },
       pupils: { include: { lessons: true } },
+      content: true
     },
   },
   user: true,
 };
 
 const TEACHERS_INCLUDE = {
-  lessons: { include: { teacher: true, pupils: true } },
+  lessons: { include: { teacher: true, pupils: true, content: true } },
   user: true,
 };
 
